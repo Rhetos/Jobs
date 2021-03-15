5514cde895d7cc22e392f07ef1c84ab20205e2cb
Damir <ddean@omega-software.hr>
2021-03-15 14:40:20 +0100

    minor fix

--- src/Rhetos.Jobs/JobExecuter.cs
+++ src/Rhetos.Jobs/JobExecuter.cs
@@ -42,15 +42,17 @@ namespace Rhetos.Jobs
 
 		private static bool JobExists(Guid jobId, string connectionString)
 		{
-			var connection = new SqlConnection(connectionString);
-			connection.Open();
-			var command = new SqlCommand($"SELECT * FROM RhetosJobs.Job WITH (READCOMMITTEDLOCK, ROWLOCK) WHERE ID = '{jobId}'", connection);
-			var reader = command.ExecuteReader();
-			var table = new DataTable();
-			table.Load(reader);
+			using (var connection = new SqlConnection(connectionString))
+			{
+				connection.Open();
+				var command = new SqlCommand($"SELECT * FROM RhetosJobs.Job WITH (READCOMMITTEDLOCK, ROWLOCK) WHERE ID = '{jobId}'", connection);
+				var reader = command.ExecuteReader();
+				var table = new DataTable();
+				table.Load(reader);
 
-			//If tranasaction that created job failed there will be no job scheduled and count will be zero
-			return table.Rows.Count > 0;
+				//If transaction that created job failed there will be no job scheduled and count will be zero
+				return table.Rows.Count > 0;
+			}
 		}
 	}
 }
