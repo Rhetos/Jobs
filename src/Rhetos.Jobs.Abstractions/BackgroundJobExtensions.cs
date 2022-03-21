74ffceec5ba99b6aa93dfe5ec585ba2726753d5e
Bojan Antolović <bojan.antolovic@gmail.com>
2022-03-21 21:55:12 +0100

    Added license headers.

--- src/Rhetos.Jobs.Abstractions/BackgroundJobExtensions.cs
+++ src/Rhetos.Jobs.Abstractions/BackgroundJobExtensions.cs
@@ -24,25 +24,25 @@ namespace Rhetos.Dom.DefaultConcepts
 {
     public static class BackgroundJobExtensions
     {
-		/// <summary>
-		/// Enqueues a DSL Action for asynchronous execution after the transaction is completed
-		/// </summary>
-		/// <param name="backgroundJob"></param>
-		/// <param name="action">
-		/// Action which should be executed.
-		/// It is instance of the action type, that contains action parameters.
-		/// </param>
-		/// <param name="executeInUserContext">
-		/// If true, Action will be executed in context of the user which started the transaction in which Action was enqueued.
-		/// Otherwise it will be executed in context of service account.
-		/// Note that the action execution will not automatically check for the user's claims.
-		/// </param>
-		/// <param name="optimizeDuplicates">
-		/// If true, previous same Actions (same Action with same parameters) within the current scope (web request) will be removed from queue.
-		/// </param>
-		/// <param name="queue">
-		/// Name of the queue. Default is null.
-		/// </param>
+        /// <summary>
+        /// Enqueues a DSL Action for asynchronous execution after the transaction is completed
+        /// </summary>
+        /// <param name="backgroundJob"></param>
+        /// <param name="action">
+        /// Action which should be executed.
+        /// It is instance of the action type, that contains action parameters.
+        /// </param>
+        /// <param name="executeInUserContext">
+        /// If true, Action will be executed in context of the user which started the transaction in which Action was enqueued.
+        /// Otherwise it will be executed in context of service account.
+        /// Note that the action execution will not automatically check for the user's claims.
+        /// </param>
+        /// <param name="optimizeDuplicates">
+        /// If true, previous same Actions (same Action with same parameters) within the current scope (web request) will be removed from queue.
+        /// </param>
+        /// <param name="queue">
+        /// Name of the queue. Default is null.
+        /// </param>
         public static void EnqueueAction(this IBackgroundJobs backgroundJob, object action, bool executeInUserContext, bool optimizeDuplicates, string queue = null)
         {
             var jobParameters = ActionJobParameter.FromActionInstance(action);
@@ -55,32 +55,32 @@ namespace Rhetos.Dom.DefaultConcepts
                 queue);
         }
 
-		/// <summary>
-		/// Schedules a recurring background job that executed the provided DSL Action.
-		/// If a recurring job with the same <paramref name="jobName"/> already exists, it will be updated.
-		/// The job will not be scheduled if the current scope (web request) fails.
-		/// </summary>
-		/// <param name="backgroundJobs"></param>
-		/// <param name="jobName">
-		/// Unique job name. If a job with the same name already exists, it will be overwritten.
-		/// </param>
-		/// <param name="action">
-		/// Action which should be executed.
-		/// It is instance of the action type, that contains action parameters.
-		/// </param>
-		/// <param name="cronExpression">
-		/// A pattern that describes the job schedule: when and how often the job is executed.
-		/// See <see href="https://en.wikipedia.org/wiki/Cron#CRON_expression"/> for basic information.
-		/// </param>
-		/// <param name="queue">
-		/// Name of the queue. Default is null.
-		/// </param>
-		public static void SetRecurringAction(this IBackgroundJobs backgroundJobs, string jobName, object action, string cronExpression, string queue = null)
-		{
-			var jobParameters = ActionJobParameter.FromActionInstance(action);
+        /// <summary>
+        /// Schedules a recurring background job that executed the provided DSL Action.
+        /// If a recurring job with the same <paramref name="jobName"/> already exists, it will be updated.
+        /// The job will not be scheduled if the current scope (web request) fails.
+        /// </summary>
+        /// <param name="backgroundJobs"></param>
+        /// <param name="jobName">
+        /// Unique job name. If a job with the same name already exists, it will be overwritten.
+        /// </param>
+        /// <param name="action">
+        /// Action which should be executed.
+        /// It is instance of the action type, that contains action parameters.
+        /// </param>
+        /// <param name="cronExpression">
+        /// A pattern that describes the job schedule: when and how often the job is executed.
+        /// See <see href="https://en.wikipedia.org/wiki/Cron#CRON_expression"/> for basic information.
+        /// </param>
+        /// <param name="queue">
+        /// Name of the queue. Default is null.
+        /// </param>
+        public static void SetRecurringAction(this IBackgroundJobs backgroundJobs, string jobName, object action, string cronExpression, string queue = null)
+        {
+            var jobParameters = ActionJobParameter.FromActionInstance(action);
 
-			backgroundJobs.SetRecurringJob<ActionJobExecuter, ActionJobParameter>(
-				jobName, cronExpression, jobParameters, queue);
-		}
-	}
+            backgroundJobs.SetRecurringJob<ActionJobExecuter, ActionJobParameter>(
+                jobName, cronExpression, jobParameters, queue);
+        }
+    }
 }
